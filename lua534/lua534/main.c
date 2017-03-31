cefa2c1cf8538ac8c27eea846cd77e635621941c
Peter Young <young40@gmail.com>
2017-03-31 22:48:53 +0800

    update

---
+++ lua534/lua534/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lua534
+//
+//  Created by Peter Young on 31/03/2017.
+//  Copyright © 2017 Peter Young. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
